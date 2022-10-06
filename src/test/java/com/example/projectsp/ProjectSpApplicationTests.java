b5af27fc694053c0c7a0e977686556d699daab0e
razvan7777 <97370543+razvan7777@users.noreply.github.com>
2022-10-06 13:21:16 +0300

    Initisl empty project.

---
+++ src/test/java/com/example/projectsp/ProjectSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
